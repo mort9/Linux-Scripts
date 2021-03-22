6239710893d0b72ece55a63c71d19c56adfdd29b
mort9 <78338218+mort9@users.noreply.github.com>
2021-03-22 05:17:36 +0000

    Update install-on-ubuntu.sh

--- shiny-server/install-on-ubuntu.sh
+++ shiny-server/install-on-ubuntu.sh
@@ -9,6 +9,7 @@ su - -c "R -e \"install.packages('shinyjs', repos='https://cran.rstudio.com/')\"
 su - -c "R -e \"install.packages('tidyverse', repos='https://cran.rstudio.com/')\""
 su - -c "R -e \"install.packages('shinydashboard', repos='https://cran.rstudio.com/')\""
 su - -c "R -e \"install.packages('shinyjs', repos='https://cran.rstudio.com/')\""
+su - -c "R -e \"install.packages('ggthemes', repos='https://cran.rstudio.com/')\""
 apt-get install gdebi-core
 wget https://download3.rstudio.org/ubuntu-14.04/x86_64/shiny-server-1.5.16.958-amd64.deb
 gdebi shiny-server-1.5.16.958-amd64.deb
